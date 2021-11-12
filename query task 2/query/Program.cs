ed7ed3783c8cf6db6afcda40871ebc42e12b998b
jabanishvili <85432293+jabanishvili@users.noreply.github.com>
2021-11-12 15:50:28 +0400

    Add files via upload

---
+++ query task 2/query/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(0, 20);
+            }
+                
+            var query = from i in numbers
+                        orderby i ascending
+                        select i;
+                      
+
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item}");
+            }
+            query = from i in numbers
+                    where i % 2== 0
+                    orderby i ascending
+                    select i;
+            Console.WriteLine("luwi");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            string[] text = { "bindis feria sofeli", "tandatan ufro", "binddeba", "ra aris chveni sicocxle", "wava da gagvifrindeba" };
+            var stringQuery = from t in text
+                              let words = t.Split('a', 'e', 'i', 'o', 'u')
+                              from word in words
+                              select word;
+
+            foreach (var item in stringQuery)
+            {
+                Console.Write(item);
+            }
+            
+
+                              
+        }
+    }
+}
---
+++ querytask/30-07task/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _30_07task
+{
+    public class Student
+    {
+        string Name { get; set; }
+        string LastName { get; set; }
+        int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"name {Name} lastname {LastName} age {Age}";
+        }
+
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<Student> students = new List<Student>
+          {
+              new Student { Name= "ani", LastName ="jbna", Age=22},
+              new Student { Name= "akaki", LastName ="shukakidze", Age=19},
+              new Student { Name= "iveri", LastName ="mamasaxlisovi", Age=22},
+              new Student { Name= "dato", LastName ="ugulava", Age=29},
+              new Student { Name= "archil", LastName ="gorgadze", Age=24},
+          };
+                var query = from i in students
+                            group i by i.Age
+                           into f
+                            orderby f.Key
+                            select f;
+                foreach (var group in query)
+                {
+                    Console.WriteLine(group.Key+ "" );
+                    foreach (var item in group)
+                    {
+                        Console.WriteLine(item.Age);
+                    }
+                }
+            }
+        }
+    }
+}
